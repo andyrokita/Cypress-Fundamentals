f34b9ee5c9ece825470a2249a82a3df0e9e7756b
andyrokita <andy.rokita@hotmail.com>
2024-08-21 09:39:55 +0100

    submit sessions test and validation

--- cypress/e2e/submitSessions.cy.js
+++ cypress/e2e/submitSessions.cy.js
@@ -1,13 +1,30 @@
 /// <reference types="cypress" />
 
 describe("Submit sessions", () => {
-  it("should navigate to submit sessions page", () => {
+  // Run before each test in this describe block
+  beforeEach(() => {
     cy.visit("conference")
     cy.get("h1").contains("View Sessions").click()
     cy.url().should("include", "/sessions")
-
     cy.get("a").contains("Submit a Session!").click()
+  })
+
+  it("should navigate to submit sessions page", () => {
     cy.url().should("include", "/sessions/new")
+  })
+
+  it("should submit a session successfully", () => {
+    //Filling the from with session info
+    cy.contains('Title').type('New session title')
+    cy.contains('Description').type('New session description')
+    cy.contains('Day').type('Friday')
+    cy.contains('Level').type('Introductory and overview')
+
+    //Submit the form
+    cy.get('form').submit()
+
+    //Validate form was submitted successfully
+    cy.contains('Session Submitted Successfully!')
 
   })
 })
