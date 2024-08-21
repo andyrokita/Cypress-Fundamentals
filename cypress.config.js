7287419f116cec82703511a8cb7120b83465aa94
andyrokita <andy.rokita@hotmail.com>
2024-08-21 08:10:35 +0100

    base url in cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,11 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    
+    env: {
+      baseUrl: 'http://loclhost:1337'
+
+    },
+
   },
 });
